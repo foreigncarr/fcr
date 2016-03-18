1720d5ad680fbaa67246578c508f1f6e473aaf59
Raymon <raymon@kakao.com>
2016-03-18 23:22:42 +0900

    og image 변경

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root 'fcr#index'
   match 'index' => 'fcr#index', via: [:get, :post]
   match 'guide' => 'fcr#rental_guide', via: [:get, :post]
-  match 'guide/condition' => 'fcr#rental_condition', via: [:get, :post]
-  match 'guide/process' => 'fcr#rental_process', via: [:get, :post]
-  match 'guide/return' => 'fcr#return_process', via: [:get, :post]
-  match 'guide/insurance' => 'fcr#insurance_guide', via: [:get, :post]
+  match 'guide_condition' => 'fcr#rental_condition', via: [:get, :post]
+  match 'guide_process' => 'fcr#rental_process', via: [:get, :post]
+  match 'guide_return' => 'fcr#return_process', via: [:get, :post]
+  match 'guide_insurance' => 'fcr#insurance_guide', via: [:get, :post]
 
   match 'admin' => 'admin#admin', via: [:get, :post]
 
@@ -14,6 +14,5 @@ Rails.application.routes.draw do
   match 'admin/load_banner_data' => 'admin#load_banner_data', via: [:get, :post]
   match 'admin/load_event_data' => 'admin#load_event_data', via: [:get, :post]
   match 'admin/update_data_model' => 'admin#update_data_model', via: [:get, :post]
-
 
 end
