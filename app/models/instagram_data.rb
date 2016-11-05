81f7a637b9745394410a90881378474800a2680e
paige.a <paige.a@daumkakao.com>
2016-11-05 15:15:18 +0900

    인스타그램 API access_token 재 발급

--- app/models/instagram_data.rb
+++ app/models/instagram_data.rb
@@ -22,7 +22,7 @@ class InstagramData
   end
 
   def self.fetch_instagram_data
-    access_token = '3618576176.38964fc.ffcf3a25cbba47f184432149fce145de'
+    access_token = '3618576176.38964fc.3f73acacef3941ab8c42ff58319fb609'
     url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=#{access_token}"
     raw = Oj.load RestClient.get url
     Hash[raw['data'].map.with_index do |h, i|
