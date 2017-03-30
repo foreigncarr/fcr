a1d54a6972429676ae1d1a9a9b160c32576200d8
paige.a <paige.a@daumkakao.com>
2017-03-30 17:27:34 +0900

    aws img cdn, ec2 주소 변경

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,7 +1,7 @@
 module ImageHelper
   def self.url(name, category=nil)
     return name if name.downcase.start_with? 'http'
-    prefix = 'http://d39imkxg0jo8t9.cloudfront.net'
+    prefix = 'http://d2jgyagfwei9sy.cloudfront.net'
     prefix = "#{prefix}/#{category.to_s}" if category
     "#{prefix}/#{name}"
   end
